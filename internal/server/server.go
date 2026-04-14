74ece7c26dd6d87c012fa6f78487fed1d6109917
duwenxin <duwenxin@google.com>
2026-04-14 00:48:49 -0400

    rename

--- internal/server/server.go
+++ internal/server/server.go
@@ -532,7 +532,7 @@ func mcpAuthMiddleware(s *Server) func(http.Handler) http.Handler {
 				return
 			}
 
-			ctx := util.WithMcpClaims(r.Context(), claims)
+			ctx := util.WithAuthTokenClaims(r.Context(), claims)
 			r = r.WithContext(ctx)
 
 			next.ServeHTTP(w, r)
